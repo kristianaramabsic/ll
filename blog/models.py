23f9a6fabcb05ccf311db5218ba6d0c9c820e55e
Kristian Arambasic <kikicic@Kristians-MacBook-Pro.local>
2020-02-16 19:22:05 +0000

    updated authorizations

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
-from labzlink import settings
 
 class Post (models.Model):
-    title = models.CharField (max_length=200)
-    author = models.ForeignKey (settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    title = models.CharField (max_length=400)
+    author = models.ForeignKey (get_user_model(), on_delete=models.CASCADE,)
     date = models.DateField (auto_now_add=True)
     text = models.TextField ()
 
     def __str__ (self):
-        return self.title [:100]
+        return self.title [:200]
 
     def get_absolute_url (self):
         return reverse ('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import (LoginRequiredMixin, UserPassesTestMixin)
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -17,17 +18,32 @@ class PostPageView (DetailView):
     model = Post
     template_name = 'posts.html'
 
-class BlogCreateView (CreateView):
+class BlogCreateView (LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['title', 'author', 'text']
+    fields = ['title', 'text']
+    login_url = 'login'
 
-class BlogUpdateView (UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class BlogUpdateView (LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'update_post.html'
     fields = ['title', 'text']
+    login_url = 'login'
 
-class BlogDeleteView (DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class BlogDeleteView (LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url = reverse_lazy('homepage')
+    login_url = 'login'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
